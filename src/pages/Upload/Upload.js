d84ddcd7ae90ac2e7d0adbe964989efc8306d7c3
Jonny Holupchinski <jonholup@gmail.com>
2017-07-19 22:17:56 -0500

    pulling in ant components

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
+import { database } from '../../config/firebase';
+import { Progress } from 'antd';
 
 export default class Upload extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      newData: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // componentDidMount() {
+  //   database.ref().on('value', snapshot => {
+  //     this.setState({
+  //       data: snapshot.val()
+  //     });
+  //   });
+  // }
+
+  handleChange(event) {
+    const newData = event.target.value;
+    this.setState({
+      newData
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    database.ref().child('images').set(this.state.newData);
+  }
   render() {
     return (
       <div>
         <h1>Upload</h1>
-        <button onClick={() => console.log('hi')} className="block">
-          Upload
-        </button>
+        <Progress id="uploader" />
+        <div>
+          {JSON.stringify(this.state.data, null, 2)}
+        </div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="file" value="file" onChange={this.handleChange} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
